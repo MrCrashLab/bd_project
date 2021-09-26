69e8e5ea19658bff7e146c59bef8add45be93da4
DanilaShikalov <dan.shikalov@gmail.com>
2021-09-26 16:58:50 +0300

    make it better

---
+++ src/main/java/test/Wellcome.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Wellcome {
+
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String hello()
+    {
+        return "Hello man";
+    }
+}
